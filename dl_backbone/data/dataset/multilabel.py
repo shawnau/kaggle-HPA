1de331c3ecb0956b11310c1eadaad176fdfb96a8
ouxiaoxuan <ouxiaoxuan@megvii.com>
2018-11-19 20:05:47 +0800

    push from corp's notebook

---
+++ dl_backbone/data/dataset/multilabel.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+
+class ProteinDataset(Dataset):
+    def __init__(self):
+        pass
